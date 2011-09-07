dadfc0c1b5ff2fc63288522eda34e591c7a2df52
Jason Gladish <jason@expectedbehavior.com>
2011-09-07 16:28:09 -0400

    clarify variable

--- lib/hydra/trace.rb
+++ lib/hydra/trace.rb
@@ -22,7 +22,7 @@ module Hydra #:nodoc:
         remote_info = @remote ? "#{REMOTE_IDENTIFIER} #{@remote} " : ''
         str = str.gsub /\n/, "\n#{remote_info}"
         str = "#{Time.now.to_f} #{remote_info}#{self.class._traceable_prefix}| #{str}"
-        IO.popen("logger", "w") { |io| io.puts str }
+        IO.popen("logger", "w") { |logger_io| logger_io.puts str }
         $stdout.puts str
       end
     end
