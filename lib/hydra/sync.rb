25c42faeff2fe06d5c15443bff0b7a7f55a20e52
Touch Ungboriboonpisal <tungbori@zynga.com>
2013-11-04 12:32:07 -0800

    fix double exit in one ssh session

--- lib/hydra/sync.rb
+++ lib/hydra/sync.rb
@@ -44,7 +44,7 @@ module Hydra #:nodoc:
 
     def sync
       # make directory to sync to
-      ssh = Hydra::SSH.new("#{@ssh_opts} #{@connect}", @remote_dir, "exit", :verbose => @verbose)
+      ssh = Hydra::SSH.new("#{@ssh_opts} #{@connect}", @remote_dir, "echo done", :verbose => @verbose)
       ssh.close
       
       #trace "Synchronizing with #{connect}\n\t#{sync_opts.inspect}"
